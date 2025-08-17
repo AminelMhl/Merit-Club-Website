b93686bca16cc5be35ab837ee196e64ad59eaf95
Mohamed Amine <mohamedamine.soltana@tbs.u-tunis.tn>
2025-08-17 10:32:39 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Text_Me_One } from "next/font/google";
+
+export default function Home() {
+  return (
+    <h1>Merit TBS is the best club ever</h1>
+  );
+}
