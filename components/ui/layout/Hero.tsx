e29621b21fa3aef1a5608aa24126250de0b45fd8
Mohamed Amine <mohamedamine.soltana@tbs.u-tunis.tn>
2025-08-17 11:47:39 +0100

    nav bar

--- components/ui/layout/Hero.tsx
+++ components/ui/layout/Hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Hero = () => {
     return (
         <div className="hero-section">
-            <h1 className="text">"Make Everything <span className="Related">Related</span> to IT"</h1>
+            <h1 className="text">Make Everything <span className="Related">Related</span> to IT</h1>
             <p className="sub-text">Dive into coding, design, and emerging
                                     tech trends in a practical, fun way</p>
             <div className="button-container">
