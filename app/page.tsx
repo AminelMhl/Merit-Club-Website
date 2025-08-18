346a4fab98cb2b7a4b6fc4d0880f46ccd4c17ec4
h4nsoo <mbhansoo05@gmail.com>
2025-08-18 21:12:51 +0100

    scroll bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
 import Hero from "@/components/ui/layout/Hero";
 import About from "./About";
-
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <About />
+      <style jsx global>{`
+        .custom-scrollbar .simplebar-scrollbar::before {
+          background-color: #fdc703 !important;
+          opacity: 0.8;
+        }
+        .custom-scrollbar .simplebar-scrollbar:hover::before {
+          background-color: #fcaa00 !important;
+          opacity: 1;
+        }
+      `}</style>
+      <SimpleBar
+        className="custom-scrollbar"
+        style={{
+          maxHeight: "100vh",
+          height: "100vh",
+          width: "100%",
+        }}
+        autoHide={true}
+      >
+        <Hero />
+        <About />
+      </SimpleBar>
     </main>
   );
 }
+
