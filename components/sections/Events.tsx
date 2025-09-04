59288c469e18f72aa6a212c61641dab56962dc13
h4nsoo <mbhansoo05@gmail.com>
2025-09-04 20:06:09 +0100

    fixed events section

--- components/sections/Events.tsx
+++ components/sections/Events.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "./Events.module.css";
-import Timeline from "../ui/Timeline";
 
 interface Event {
   id: number;
@@ -11,29 +10,30 @@ interface Event {
   image: string;
 }
 
-const Events = () => {
+const Events: React.FC = () => {
   const events: Event[] = [
     {
       id: 1,
-      title: "FreelaceIT 3.0",
-      date: "March 2024",
-      description: "Explore freelancing and remote work in the field of IT",
+      title: "FreelanceIT 3.0",
+      date: "December 2024",
+      description:
+        "Explore freelancing and remote work in the field of IT with industry experts and networking opportunities.",
       image: "/Hero.png",
     },
     {
       id: 2,
       title: "Web Development Competition",
-      date: "April 2024",
+      date: "February 2025",
       description:
-        "Contestants try to build the most innovative web application in a 3 hours time span",
+        "Contestants try to build the most innovative web application in a 3 hours time span.",
       image: "/about1.png",
     },
     {
       id: 3,
       title: "Code in the Dark",
-      date: "May 2024",
+      date: "May 2025",
       description:
-        "Contestants try to recreate a given picture of a web application.",
+        "Contestants try to recreate a given picture of a web application without checking their output.",
       image: "/about2.png",
     },
   ];
@@ -45,9 +45,22 @@ const Events = () => {
         <p className={styles.subtitle}>
           Explore our journey through past workshops, bootcamps, and tech events
         </p>
+      </div>
 
-        <Timeline />
-    </div>
+      <section className={styles.timeline}>
+        <ol className={styles.timelineList}>
+          {events.map((event, index) => (
+            <li key={event.id} className={styles.timelineItem}>
+              <div className={styles.timelineContent}>
+                <time className={styles.timelineYear}>{event.date}</time>
+                <h3 className={styles.eventTitle}>{event.title}</h3>
+                <p className={styles.eventDescription}>{event.description}</p>
+              </div>
+            </li>
+          ))}
+          <li className={styles.timelineEnd} />
+        </ol>
+      </section>
     </div>
   );
 };
